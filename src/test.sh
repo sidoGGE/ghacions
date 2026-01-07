f7dc58dc53e99ff184f12ca28983155bbba8e5e0
sidoGGE <sidobdh@gmail.com>
2026-01-07 19:05:18 +0100

    create py app_1

--- src/test.sh
+++ src/test.sh
@@ -6,6 +6,6 @@ if [ "$OUTPUT" = "$EXPECTED" ]; then
     echo "🟢Test passed"
     exit 0
 else
-    echo "🔴Test failed: expected '$EXPECTED' but got '$OUTPUT'"
+    echo "🔴 Test failed: expected '$EXPECTED' but got '$OUTPUT'"
     exit 1
 fi
