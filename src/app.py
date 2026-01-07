d9c8e7ed6b9f9668a8fa061ca95e3deb755d3544
sidoGGE <sidobdh@gmail.com>
2026-01-07 18:35:38 +0100

    create py app

---
+++ src/app.py
@@ -0,0 +1,4 @@
+def gree(name):
+    return f"Hello, {name}!"
+a = gree("World")
+print(a)
---
+++ src/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#src/test.sh
+EXPECTED="hello, Test"
+OUTPUT=$(python src/app.py)
+if [ "$OUTPUT" = "$EXPECTED" ]; then
+    echo "Test passed"
+    exit 0
+else
+    echo "Test failed: expected '$EXPECTED' but got '$OUTPUT'"
+    exit 1
+fi
